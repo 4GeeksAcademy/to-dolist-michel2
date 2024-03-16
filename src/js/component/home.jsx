7e55e7c56992fb31f38ff8420c4b27f63ebd7d08
Michel Rivero <157402536+michelriv@users.noreply.github.com>
2024-03-16 00:15:07 +0000

    finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,59 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 //include images into your bundle
+
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
 
+//create your first component
+
+const Home = () => {
+    const [inputValue, setInputValue] = useState("");
+    const [todos, setTodos] = useState([]);
+
+    return (
+        <div className="small-container">
+            <h1>My Todos</h1>
+            <ul>
+                <li>
+                    <input
+                        type="text"
+                        onChange={(e) => setInputValue(e.target.value)}
+                        value={inputValue}
+                        onKeyPress={(e) => {
+                            if (e.key === "Enter") {
+                                setTodos([...todos, inputValue]);
+                                setInputValue("");
+                            }
+                        }}
+                        placeholder="What do you need to do?"
+                    />
+                </li>
+                {todos.map((item, index) => (
+                    <li key={index}>
+                        {item}
+                        <span
+                            className="x"
+                            onClick={() => setTodos(todos.filter((_, currentIndex) => index !== currentIndex))}
+                        >
+                            🅧
+                        </span>
+                    </li>
+                ))}
+            </ul>
+			<span class="bottom-text">13 items left</span>
+        </div>
+    );
+}
+			
+			
+				
+			
+			
+		
+	
+		
+
+				
 export default Home;
