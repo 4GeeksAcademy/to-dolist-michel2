56fc36ca9169f530a565d8f0c40f657fc4bb2d9e
Michel Rivero <157402536+michelriv@users.noreply.github.com>
2024-03-20 22:27:54 +0000

    .

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -12,6 +12,28 @@ const Home = () => {
     const [inputValue, setInputValue] = useState("");
     const [todos, setTodos] = useState([]);
 
+    fetch('https://playground.4geeks.com/apis/fake/todos/user/michelriv', {
+      method: "PUT",
+      body: JSON.stringify(todos),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(resp => {
+        console.log(resp.ok); // Will be true if the response is successful
+        console.log(resp.status); // The status code=200 or code=400 etc.
+        console.log(resp.text()); // Will try to return the exact result as a string
+        return resp.json(); // (returns promise) Will try to parse the result as JSON and return a promise that you can .then for results
+    })
+    .then(data => {
+        // Here is where your code should start after the fetch finishes
+        console.log(data); // This will print on the console the exact object received from the server
+    })
+    .catch(error => {
+        // Error handling
+        console.log(error);
+    });
+
     return (
         <div className="small-container">
             <h1>My Todos</h1>
@@ -43,6 +65,8 @@ const Home = () => {
                 ))}
             </ul>
 			<span class="bottom-text">13 items left</span>
+
+            
         </div>
     );
 }
